cd119b699863623de95be0a3d17bc95b1182e21d
Silvio Massari <silvio@kidozen.com>
2015-07-03 16:49:13 -0300

    new option 'level' was added

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ var consoleLogger = require("./lib/consoleLogger");
 module.exports.create = function (opts) {
 
     opts = opts || {};
-    if (typeof opts !== "object") throw new Error("Invalid options argument, it must be an object instance.");
+    if (Array.isArray(opts) || typeof opts !== "object") throw new Error("Invalid options argument, it must be an object instance.");
 
     // clone options
     var options = {
         logger: opts.logger || fakeLogger,
-        header: opts.header || "x-timeout"
+        header: opts.header || "x-timeout",
+        level: opts.level   || "verbose"
     };
 
     // validate logger
@@ -21,11 +22,15 @@ module.exports.create = function (opts) {
         throw new Error("Invalid options.logger property. [true/false/logger instance like winston]");
     }
 
+    // valdate log level
+    if (typeof options.header !== "string") throw new Error("Invalid 'options.header' property, it must be a string. Default is 'x-timeout'");
+
     // valdate header name
-    options.header = options.header;
-    if (typeof options.header !== "string") throw new Error("Invalid 'options.header' property, it must be a string.");
+    if (typeof options.level !== "string") throw new Error("Invalid 'options.level' property, it must be a string. Default is 'verbose'");
 
     // create instance
+    options.logger.info("http-socket-hang-up options were validated.");
+
     var instance = new TimeoutMiddleware(options);
     return instance.callback;
 };
@@ -33,6 +38,8 @@ module.exports.create = function (opts) {
 function TimeoutMiddleware(options) {
 
     options.logger.info("http-socket-hang-up middleware initialized", { header: options.header });
+
+    var log = options.logger[options.level];
 
     this.callback = function callback(req, res, next) {
 
@@ -45,19 +52,19 @@ function TimeoutMiddleware(options) {
 
         if (context.timeout < context.keepAlive) {
             // nothing to do because TCP Keep alive is long enough
-            options.logger.verbose("TCP Keep Alive was not updated", context);
+            log("TCP Keep Alive was not updated", context);
 
         } else {
 
             // timeout is longer than TCP keep alive.
             // we must change TCP keep alive value to a new value greater than the timeout
             context.newKeepAlive = context.timeout + context.keepAlive;
-            options.logger.verbose("TCP Keep Alive was updated with a new value:", context);
+            log("TCP Keep Alive was updated with a new value:", context);
             req.connection.setTimeout(context.newKeepAlive);
 
             // subscribe to response's 'finish' event in order to reset TCP keep alive to its previous value.
             res.on("finish", function () {
-                options.logger.verbose("TCP keep alive was reseted to its original value.", context);
+                log("TCP keep alive was reseted to its original value.", context);
                 req.connection.setTimeout(context.keepAlive);
             });
         };
--- test/index.js
+++ test/index.js
@@ -1,5 +1,7 @@
 "use strict";
 var assert = require("assert");
+var sinon = require("sinon");
+var rewire = require("rewire");
 
 describe("index.js", function () {
 
@@ -9,5 +11,52 @@ describe("index.js", function () {
         assert.equal(typeof middleware.create, "function");
     });
 
-    // more test comming soon :)
+    describe("create method:", function() {
+
+        it("should fails when options are invalid", function () {
+            var middleware = require("..");
+
+            var invalidOptions = [
+                100,
+                true,
+                "foo",
+                []
+            ];
+
+            invalidOptions.forEach(function (options) {
+                try {
+                    middleware.create(options);
+                    assert.fail("Did not fail!");
+                } catch (e) {
+                    assert.ok(e instanceof Error);
+                    assert.equal(e.message, "Invalid options argument, it must be an object instance.");
+                }
+            });
+        });
+
+        describe("logger option.", function () {
+
+            it("It should set fakeLogger by default", function() {
+                var middleware = rewire("..");
+                var fakeLogger = {
+                    info: sinon.stub()
+                };
+
+                middleware.__set__("fakeLogger", fakeLogger);
+                middleware.create();
+
+                assert.ok(fakeLogger.info.called);
+            });
+
+            it("It should set the console as logger when the option was set to true", function() {
+                var middleware = require("..");
+                var consoleStub = sinon.spy(console, "log");
+
+                middleware.create();
+
+                assert.ok(consoleStub);
+
+            });
+        });
+    });
 });
